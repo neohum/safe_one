4de047ef6c065b9cff59c32dbf21fb0142c91765
neohum <neohum77@gmail.com>
2025-03-28 20:10:10 +0900

    최종결과물2 (#1)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,22 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireScripts()
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18]">
+    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] min-h-screen flex flex-col p-6 lg:p-8">
+        <div class="w-full p-4 mb-8 bg-white dark:bg-zinc-900">
+            <div class="flex items-center justify-between gap-4">
+                <a href="{{ route('index') }}" class="flex-shrink-0">
+                    <img src="{{ asset('image/home2.png') }}" alt="logo" class="object-contain h-16" />
+                </a>
+                <div class="flex gap-4">
+                    <a href="{{ route('index') }}">
+                        <img src="{{ asset('image/change2.png') }}" alt="change" class="object-contain h-16" />
+                    </a>
+                    <a href="{{ route('youtube-select') }}">
+                        <img src="{{ asset('image/select2.png') }}" alt="select" class="object-contain h-16 transition-opacity cursor-pointer hover:opacity-80" />
+                    </a>
+                </div>
+            </div>
+        </div>
         @livewire('welcome')
     </body>
 </html>
